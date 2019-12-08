f1a603a01bbe57281135b1686ae74da2b9c5eb90
Jukka <jukka1993@foxmail.com>
2019-12-08 18:48:36 +0800

    maxsubseq

--- mooc/maxsubseq/maxsubseq.js
+++ mooc/maxsubseq/maxsubseq.js
@@ -2,7 +2,7 @@
 // var data = [1, 2, -2, 3, 4, 5, -4, 6, -7, 9, 22];
 
 var data = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
-function maxsubseqfunc1(arr) {  // O(n^3)
+function maxsubseqfunc1(arr) {  // O(n^3) 暴力
     var max = -Infinity;
     for (var i = 0; i < arr.length; i++) {
         for (var j = i; j < arr.length; j++) {
@@ -17,7 +17,7 @@ function maxsubseqfunc1(arr) {  // O(n^3)
     }
     return max;
 }
-function maxsubseqfunc2(arr) {  // O(n^2)
+function maxsubseqfunc2(arr) {  // O(n^2) 暴力上的优化
     var max = -Infinity;
     for (var i = 0; i < arr.length; i++) {
         var sum = 0;
@@ -35,14 +35,12 @@ function maxsubseqfunc2(arr) {  // O(n^2)
 function maxsubseqfunc3(arr, begin, end) { // O(Nlog(N)) 分治
     if (begin >= end) {
         var finalMax1 = arr[begin];
-        console.log(begin, end, finalMax1);
         return finalMax1;
     }
     var mid = Math.floor((begin + end) / 2);
-    console.log("begin,mid,end", begin, mid, end);
-    var leftMax = maxsubseqfunc3(arr, begin, mid - 1);
-    var rightMax = maxsubseqfunc3(arr, mid + 1, end);
-    var crossMax = 0;
+    var leftMax = maxsubseqfunc3(arr,begin,mid-1);
+    var rightMax = maxsubseqfunc3(arr,mid+1,end);
+    var crossMax = arr[mid];
     var sum = arr[mid];
     for (var i = 1; i <= mid - begin; i++) {
         sum += arr[mid - i];
@@ -50,6 +48,7 @@ function maxsubseqfunc3(arr, begin, end) { // O(Nlog(N)) 分治
             crossMax = sum;
         }
     }
+    sum = crossMax;
     for (var j = 1; j <= end - mid; j++) {
         sum += arr[mid + j];
         if (sum > crossMax) {
@@ -57,11 +56,25 @@ function maxsubseqfunc3(arr, begin, end) { // O(Nlog(N)) 分治
         }
     }
     var finalMax = Math.max(leftMax, rightMax, crossMax);
-    console.log(begin, end, finalMax);
     return finalMax;
+}
+function maxsubseqfunc4(arr){   // O(n) 在线处理法
+    var sum = 0;
+    var max = -Infinity;
+    for(var i = 0;i < arr.length;i++){
+        sum += arr[i];
+        if(sum > max){
+            max = sum;
+        } 
+        if(sum<0){
+            sum = 0;
+        }
+    }
+    return max;
 }
 
 console.log(maxsubseqfunc1(data));
 console.log(maxsubseqfunc2(data));
 console.log(maxsubseqfunc3(data, 0, data.length - 1));
+console.log(maxsubseqfunc4(data));
 
